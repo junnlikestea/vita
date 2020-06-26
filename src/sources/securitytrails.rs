0042c03d55c70a155326ee7e05c34fc6a5f00066
Junn <likesgophers@gmail.com>
2020-06-26 12:26:41 +1000

    add security trails & sublister

---
+++ src/sources/securitytrails.rs
@@ -0,0 +1,72 @@
+use dotenv::dotenv;
+use http_types::headers;
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::env;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Deserialize)]
+struct SecTrailsResult {
+    subdomains: Vec<String>,
+}
+
+fn build_url(host: &str) -> String {
+    format!(
+        "https://api.securitytrails.com/v1/domain/{}/subdomains",
+        host
+    )
+}
+
+pub async fn run(host: &str) -> Result<HashSet<String>> {
+    dotenv().ok();
+    let api_key = env::var("SECURITY_TRAILS_KEY")
+        .expect("SECURITY_TRAILS_KEY must be set to use Security Trails API");
+    let mut results = HashSet::new();
+    let uri = build_url(host);
+    let resp: Option<SecTrailsResult> = surf::get(uri)
+        .set_header("apikey", api_key)
+        .recv_json()
+        .await?;
+    // secuirity trails doesn't add the host name to the result.
+    // so api.hackerone.com will just be api in the results.
+    // we will add the hostname manually to result.
+    match resp {
+        Some(d) => d
+            .subdomains
+            .into_iter()
+            .map(|s| results.insert(format!("{}.{}", s, &host)))
+            .for_each(drop),
+        None => eprintln!("Security Trails couldn't find any results for: {}", &host),
+    }
+
+    Ok(results)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures_await_test::async_test;
+
+    #[test]
+    fn url_builder() {
+        let correct_uri = "https://api.securitytrails.com/v1/domain/hackerone.com/subdomains";
+        assert_eq!(correct_uri, build_url("hackerone.com"));
+    }
+
+    // Checks to see if the run function returns subdomains
+    #[ignore]
+    #[async_test]
+    async fn returns_results() {
+        let results = run("hackerone.com").await.unwrap();
+        assert!(results.len() > 0);
+    }
+
+    #[ignore]
+    #[async_test]
+    async fn handle_no_results() {
+        let host = "hdsad.com";
+        let results = run(host).await.unwrap();
+        assert!(results.len() == 0);
+    }
+}
---
+++ src/sources/sublister.rs
@@ -0,0 +1,53 @@
+use serde_json::value::Value;
+use std::collections::HashSet;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn build_url(host: &str) -> String {
+    format!("https://api.sublist3r.com/search.php?domain={}", host)
+}
+
+pub async fn run(host: &str) -> Result<HashSet<String>> {
+    let mut results = HashSet::new();
+    let uri = build_url(host);
+    let resp: Option<Value> = surf::get(uri).recv_json().await?;
+    //TODO: isn't there a more efficient way to do this (complexity wise)
+    // not just this source, but multiple sources have unecessary loops.
+    match resp {
+        Some(d) => {
+            let arr = d.as_array().unwrap();
+            arr.into_iter()
+                .map(|s| results.insert(s.as_str().unwrap().into()))
+                .for_each(drop);
+        }
+        None => eprintln!("Sublist3r couldn't find any results for: {}", &host),
+    }
+
+    Ok(results)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures_await_test::async_test;
+
+    #[test]
+    fn url_builder() {
+        let correct_uri = "https://api.sublist3r.com/search.php?domain=hackerone.com";
+        assert_eq!(correct_uri, build_url("hackerone.com"));
+    }
+
+    // Checks to see if the run function returns subdomains
+    #[async_test]
+    async fn returns_results() {
+        let results = run("hackerone.com").await.unwrap();
+        assert!(results.len() > 0);
+    }
+
+    #[async_test]
+    async fn handle_no_results() {
+        let host = "hdsad.com";
+        let results = run(host).await.unwrap();
+        assert!(results.len() == 0);
+    }
+}
