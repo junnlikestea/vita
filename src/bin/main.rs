8bd552377418014cb6461c5ac3abc02222d3a572
Junn <likesgophers@gmail.com>
2020-06-26 22:41:07 +1000

    add cli tool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,72 @@
 extern crate vita;
 use self::vita::*;
+use clap::{App, Arg};
+use std::fs;
+use std::io::{self, Read};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+fn create_clap_app(version: &str) -> clap::App {
+    // Add support to not include subdomains.
+    let app = App::new("vita")
+        .version(version)
+        .about("Gather subdomains from passive sources")
+        .usage("vita <domain.com>")
+        .arg(Arg::with_name("input").index(1).required(false))
+        .arg(
+            Arg::with_name("file")
+                .help("vita -f <roots.txt>")
+                .short("f")
+                .long("file"),
+        )
+        .arg(
+            Arg::with_name("domain")
+                .help("vita -d domain.com")
+                .short("d")
+                .long("domain"),
+        )
+        .arg(
+            Arg::with_name("all_sources")
+                .help("use sources which require an Api key")
+                .short("a")
+                .long("all"),
+        );
+
+    app
+}
+
+fn read_stdin() -> Result<Vec<String>> {
+    let mut buffer = String::new();
+    let mut res = Vec::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    for line in buffer.split_whitespace() {
+        res.push(line.to_string())
+    }
+    Ok(res)
+}
 
 #[async_std::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let hosts: Vec<String> = vec!["starbucks.com".to_string()];
-    let results = vita::runner(hosts).await;
+async fn main() -> Result<()> {
+    let args = create_clap_app("v0.1.0");
+    let matches = args.get_matches();
+    let mut all_sources = false;
+    let mut hosts: Vec<String> = Vec::new();
+
+    if matches.is_present("all_sources") {
+        all_sources = true;
+    }
+
+    if matches.is_present("file") {
+        let input = matches.value_of("input").unwrap();
+        let contents = fs::read_to_string(input)?;
+        hosts = contents.lines().map(|l| l.to_string()).collect();
+    } else if matches.is_present("domain") {
+        hosts.push(matches.value_of("input").unwrap().to_string());
+    } else {
+        hosts = read_stdin()?;
+    }
+
+    let results = vita::runner(hosts, all_sources).await;
     for r in results {
         println!("{}", r);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ async fn free_sources(host: String) -> HashSet<String> {
         Box::pin(virustotal::run(host.to_owned())),
         Box::pin(threatminer::run(host.to_owned())),
         Box::pin(sublister::run(host.to_owned())),
+        Box::pin(wayback::run(host.to_owned())),
         Box::pin(hackertarget::run(host)),
     ];
 
@@ -56,21 +57,25 @@ async fn all_sources(host: String) -> HashSet<String> {
     }
 
     let res = join_all(tasks).await;
-    res.into_iter().flat_map(|v| v).flatten().collect()
+    res.into_iter().flatten().flatten().collect()
 }
 
 // Takes a bunch of hosts and collects data on them
-pub async fn runner(hosts: Vec<String>) -> Vec<String> {
+pub async fn runner(hosts: Vec<String>, all: bool) -> Vec<String> {
     const ACTIVE_REQUESTS: usize = 50;
     use futures::stream::StreamExt;
 
-    let responses = futures::stream::iter(
-        hosts
-            .into_iter()
-            .map(|host| task::spawn(async { free_sources(host).await })),
-    )
+    let responses = futures::stream::iter(hosts.into_iter().map(|host| {
+        task::spawn(async move {
+            if all {
+                all_sources(host).await
+            } else {
+                free_sources(host).await
+            }
+        })
+    }))
     .buffer_unordered(ACTIVE_REQUESTS)
     .collect::<Vec<HashSet<String>>>();
 
-    responses.await.into_iter().flat_map(|v| v).collect()
+    responses.await.into_iter().flatten().collect()
 }
